b9e41511b91691b95362c30f75d51562c76a4b06
teixeirajrwd <teixeirajrwd@gmail.com>
2023-10-09 14:50:17 -0300

    Primeiro commit

---
+++ src/components/Container/index.js
@@ -0,0 +1,9 @@
+import styles from "./Container.module.css";
+function Container(){
+    return(
+        <div className={styles.container}></div>
+    );
+
+}
+
+export default Container;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css"
+function Footer (){
+    return(
+        <footer className={styles.footer}>
+            Desenvolvido por Jun1or Te1xe1ra.
+        </footer>
+    );
+}
+
+export default Footer;
